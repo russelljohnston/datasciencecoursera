774a93ee933393bd43e20d8635c06684e40929cf
russelljohnston <rwi.johnston@gmail.com>
2015-11-21 18:59:10 +0000

    corrections made

--- cleaningdatamod/Week3/run_analysis.R
+++ cleaningdatamod/Week3/run_analysis.R
@@ -1,44 +1,55 @@
 library(dplyr)
+library(tidyr)
 
-#Read data to data frame
+#Reading from dir data/ into dataframes
+test_xDF <-   read.table("data/X_test.txt",header=FALSE,quote="")
+test_yDF <-   read.table("data/y_test.txt",header=FALSE,quote="")
+sub_testDF <- read.table("data/subject_test.txt",header=FALSE,quote="")
 
-labels <- read.table("UCI_HAR_Dataset/features.txt",header=FALSE)
+activity_labels <- read.table("data/activity_labels.txt",header=FALSE)
+names(activity_labels) <- c("activity","label")
+
+train_xDF <-   read.table("data/X_train.txt",header=FALSE,quote="")
+train_yDF <-   read.table("data/y_train.txt",header=FALSE,quote="")
+sub_trainDF <- read.table("data/subject_train.txt",header=FALSE,quote="")
+
+#Merging  DATA SETS
+testJoin <- cbind(test_yDF,sub_testDF,test_xDF)
+trainJoin <- cbind(train_yDF,sub_trainDF,train_xDF)
+MergedData <- rbind(testJoin, trainJoin)
+
+#Adding header 
+labels <- read.table("data/features.txt",header=FALSE)
 labels.T <- t(labels[c(2)])
-
-
-test_xDF <-   read.table("UCI_HAR_Dataset/test/X_test.txt",header=FALSE,quote="")
 names(test_xDF) <- labels.T
-test_yDF <-   read.table("UCI_HAR_Dataset/test/y_test.txt",header=FALSE,quote="")
-names(test_yDF) <- c("y_test")
-sub_testDF <- read.table("UCI_HAR_Dataset/test/subject_test.txt",header=FALSE,quote="")
-names(sub_testDF) <- c("subject_test")
+names(MergedData) <- c("activity","subject",labels.T)
 
-train_xDF <-   read.table("UCI_HAR_Dataset/train/X_train.txt",header=FALSE,quote="")
-names(train_xDF) <- labels.T
-train_yDF <-   read.table("UCI_HAR_Dataset/train/y_train.txt",header=FALSE,quote="")
-names(train_yDF) <- c("y_train")
-sub_trainDF <- read.table("UCI_HAR_Dataset/train/subject_train.txt",header=FALSE,quote="")
-names(sub_trainDF) <- c("subject_train")
+#Merge table with the activity text labels:
+MergedData <- merge(MergedData, activity_labels, by = "activity") 
 
-
-#JOINING TEST DATA SETS
-testJoin <- cbind(test_xDF,test_yDF,sub_testDF)
-#adding extra rows of NA to match number of rows of training dataset
-na.count <- nrow(train_xDF) - nrow(testJoin)
-na.dm <- matrix(NA, na.count, ncol(testJoin))
-colnames(na.dm) <- colnames(testJoin)
-testJoin <- rbind(testJoin, na.dm)
-# testJoin$ID<-seq.int(nrow(testJoin)) # create column of index
-
-#JOINING TRAIN DATA SETS
-trainJoin <- cbind(train_xDF,train_yDF,sub_trainDF)
-##JOINING TRAIN-TEST DATA SETS
-JoinedData <- cbind(trainJoin,testJoin)
+#Selecting mean and standard deviation based on following text pattern:
+MeanStd <- MergedData[,grepl("subject", colnames(MergedData)) 
+                         | grepl("label", colnames(MergedData)) 
+                         | grepl("mean\\(", colnames(MergedData)) 
+                         | grepl("std\\(", colnames(MergedData))]
 
 
-extract <- JoinedData[c(1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 227:228, 240:241, 253:254, 266:271, 345:350, 424:429, 503:504, 516:517, 529:530, 542:543)]
 
-extractS<-testJoin[,grepl("mean()$", colnames(testJoin)) | grepl("^std()$", colnames(testJoin))]
+#Tidying the table header labels a little. The codebook.txt should provide 
+#more details regarding interprating the labeling convention:
+names(MeanStd) <- tolower(names(MeanStd))
+names(MeanStd) <-  names(MeanStd) %>%
+  gsub("\\()", "", .) %>%
+  gsub("-", "", .) %>%
+  gsub("std", "standarddeviation", .)
 
+#Now computing the mean grouped by subject and activity (label):
+tidyDF <- tbl_df(MeanStd) %>%
+  gather(measurement, value, -subject, -label) %>%
+  group_by(subject, label, measurement) %>% 
+  summarise(mean=mean(value))
+
+#Finally, write this new tidy data set to a file.
+write.table(tidyDF, "tidyData.txt",sep="\t", quote=FALSE, row.name=FALSE)
 
 
